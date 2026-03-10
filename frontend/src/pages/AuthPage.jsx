6fa1fc32766a6a04ada37dbc926fba87812c2673
Harshavardhan Devang <harshavardhandevang@gmail.com>
2026-03-10 11:38:43 +0530

    changes

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "http://localhost:5000";
 
 function AuthPage({ onAuthSuccess }) {
   const [authMode, setAuthMode] = useState("login");
